60c9843d6a33504d0197c97c4e3a0eb7721d572d
Powerveil <1968103220@qq.com>
2024-01-22 15:43:14 +0800

    配置swagger

--- power-security-demo/src/main/java/com/power/DemoApplication.java
+++ power-security-demo/src/main/java/com/power/DemoApplication.java
@@ -3,12 +3,14 @@ package com.power;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Powerveil
  * @Date 2024/1/18 16:02
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源自动配置
+@EnableSwagger2
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class);
